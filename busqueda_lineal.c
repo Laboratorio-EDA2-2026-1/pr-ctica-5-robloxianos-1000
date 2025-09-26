7d788759f4c352ec4333657b3f3d709c6aff92b5
Uriel <urielrangell435@gmail.com>
2025-09-26 08:59:33 -0600

    Update busqueda_lineal.c

--- busqueda_lineal.c
+++ busqueda_lineal.c
@@ -37,6 +37,37 @@ int busquedaLienalMejorada(int arreglo[], int tamaño, int objetivo) {
 
 
 
+int busquedaLinealRecursiva(int arreglo[], int tamaño, int valor, int indice) {
+    if (indice >= tamaño)          
+        return -1;
+    if (arreglo[indice] == valor)  
+        return indice;
+    
+    return busquedaLinealRecursiva(arreglo, tamaño, valor, indice + 1);
+}
+
+
+
+
+int busquedaLinealCentinela(int arreglo[], int tamaño, int valor) {
+    int ultimo = arreglo[tamaño - 1]; 
+    arreglo[tamaño - 1] = valor;      
+
+    int i = 0;
+    while (arreglo[i] != valor) {
+        i++;
+    }
+
+    arreglo[tamaño - 1] = ultimo;
+
+    if (i < tamaño - 1 || arreglo[tamaño - 1] == valor) {
+        return i;  
+    } else {
+        return -1; 
+    }
+}
+
+
 
 
 int main() {
@@ -51,7 +82,32 @@ int main() {
     printf("BUSQUEDA LINEAL MEJORADO\n");
     int resultado2 = busquedaLienalMejorada(arreglo, tamaño, objetivo);
     printf(" %i\n", resultado2);
+
+
+       int numeros[] = {5, 3, 8, 6, 2, 9, 8};
+    int buscado = 8;
+
+    int posicion = busquedaLinealRecursiva(numeros, tamaño, buscado, 0);
+
+    if (posicion != -1)
+        printf("el numero %i se encontro en la posicion %i\n", buscado, posicion);
+    else
+        printf("el numero %i no se encontro en el arreglo\n", buscado);
+
     
+
+    int posicion = busquedaLinealCentinela(numeros, tamaño, buscado);
+
+    if (posicion != -1)
+        printf("el numero %i se encontro en la posicion %i\n", buscado, posicion);
+    else
+        printf("el numero %i no se encontro en el arreglo\n", buscado);
+
+
+
+
+       
+
     return 0;
 }
 
