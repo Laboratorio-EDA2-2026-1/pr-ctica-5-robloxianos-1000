aaa979460df4cedec116f3897e2a8ffbb7041b44
DianVal47 <dianasestradar@gmail.com>
2025-09-26 08:46:22 -0600

    Update counting_sort.c

--- counting_sort.c
+++ counting_sort.c
@@ -1,4 +1,46 @@
 // Escribe aquí la implementación de los ejercicios de Counting Sort.
+EJERCICIO 1
+    #include <stdio.h>
+#include <string.h>
+
+int main() {
+    char A[100];             
+    int B[100];              
+    int C[26];               
+    int n, k;
+
+    printf("Escribe una palabra (solo minusculas): ");
+    scanf("%s", A);
+
+    n = strlen(A);
+    k = 25;
+
+    for (int i = 0; i <= k; i++) {
+        C[i] = 0;
+    }
+    for (int j = 0; j < n; j++) {
+        C[A[j] - 'a'] = C[A[j] - 'a'] + 1;
+    }
+    for (int i = 1; i <= k; i++) {
+        C[i] = C[i] + C[i - 1];
+    }
+    for (int j = n - 1; j >= 0; j--) {
+        int pos = A[j] - 'a';
+        B[C[pos] - 1] = A[j];
+        C[pos] = C[pos] - 1;
+    }
+    printf("Ordenada: ");
+    for (int i = 0; i < n; i++) {
+        printf("%c", B[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
+
+
+
+EJERCICIO 2
 #include <stdio.h>
 #include <stdlib.h>
 
